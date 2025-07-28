83e67ed53ea0d01422d31f78e1b85000cbab00cd
Naama Deshet <naamayad2@gmail.com>
2025-07-29 01:00:38 +0300

    Added Joomla site files

---
+++ site-files/html/administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-07-28 19:34:02 UTC
+#Software: Joomla! 5.3.2 Stable [ Timu ] 8-July-2025 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2025-07-28T19:34:02+00:00	INFO 172.19.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2025-07-28T19:54:59+00:00	INFO 172.19.0.1	joomlacanceled	
+2025-07-28T19:55:15+00:00	INFO 172.19.0.1	joomlacanceled	
+2025-07-28T19:56:51+00:00	INFO 172.19.0.1	joomlacanceled	
